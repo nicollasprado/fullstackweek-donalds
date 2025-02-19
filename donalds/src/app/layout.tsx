205714b10b7f30057ef21466060cf62ff22cf244
nicollasprado <nicollasprado.profissional@gmail.com>
2025-02-19 20:36:44 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- donalds/src/app/layout.tsx
+++ donalds/src/app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -18,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${poppins.className} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-br">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
 }
